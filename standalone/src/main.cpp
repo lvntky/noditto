4ca485f394cb9806d46eb9dcc2c9fc9e044245c8
lvntky <klevent1903@gmail.com>
2024-07-18 17:07:28 +0300

    [feature] add file loading

--- standalone/src/main.cpp
+++ standalone/src/main.cpp
@@ -1,9 +1,31 @@
+#include <fstream>
+#include <iostream>
 #include <noditto/commons/log_commons.hpp>
 #include <noditto/lexer/lexer.hpp>
 #include <noditto/tostring/tostring.hpp>
 
-int main() {
-  Lexer lexer("int a = 10;");
+std::string readFileToString(const std::string& filename) {
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    std::cerr << "Error opening file: " << filename << std::endl;
+    return "";
+  }
+
+  std::string content((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
+
+  file.close();
+  return content;
+}
+
+int main(int argc, char** argv) {
+  if (argc < 1) {
+    spdlog::error("error");
+    throw std::runtime_error("Provide file to check: ./Noditto <file.cpp>");
+  }
+
+  std::string fileToheck = readFileToString(argv[1]);
+
+  Lexer lexer(fileToheck);
   std::vector<token_t> tokens = lexer.tokenize();
 
   for (size_t i = 0; i < tokens.size(); i++) {
